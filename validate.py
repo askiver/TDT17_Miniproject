15624b4e8a32dcf887c6e97e094efb6c81e5026f
askis <askis@stud.ntnu.no>
2024-11-27 21:57:32 +0100

    Project finished

--- validate.py
+++ validate.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('runs/detect/train2/weights/best.pt')
+    model = YOLO('runs/detect/train7/weights/best.pt')
 
     metrics = model.val(split='test')
     print(metrics)
